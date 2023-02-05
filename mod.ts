deb53d586c69aea414928708bae7b06c3877c24d
linbingquan <695601626@qq.com>
2023-02-05 22:17:26 +0800

    add github urls

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 const urls = [
+  "alive.github.com",
   "api.github.com",
   "assets-cdn.github.com",
   "avatars.githubusercontent.com",
@@ -10,7 +11,9 @@ const urls = [
   "avatars5.githubusercontent.com",
   "camo.githubusercontent.com",
   "central.github.com",
+  "cloud.githubusercontent.com",
   "codeload.github.com",
+  "collector.github.com",
   "desktop.githubusercontent.com",
   "favicons.githubusercontent.com",
   "gist.github.com",
@@ -19,16 +22,22 @@ const urls = [
   "github-production-release-asset-2e65be.s3.amazonaws.com",
   "github-production-repository-file-5c1aeb.s3.amazonaws.com",
   "github-production-user-asset-6210df.s3.amazonaws.com",
+  "github.blog",
   "github.com",
   "github.community",
+  "github.dev",
   "github.githubassets.com",
   "github.global.ssl.fastly.net",
   "github.io",
   "github.map.fastly.net",
   "githubstatus.com",
+  "live.github.com",
   "media.githubusercontent.com",
+  "objects.githubusercontent.com",
+  "pipelines.actions.githubusercontent.com",
   "raw.githubusercontent.com",
   "user-images.githubusercontent.com",
+  "vscode.dev",
 ];
 
 const promises = urls.map(async (url) => {
