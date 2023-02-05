47dd953dd2d77d2523f7e8b641c4614e60c6758d
linbingquan <695601626@qq.com>
2023-02-05 18:15:51 +0800

    init project

---
+++ mod.ts
@@ -0,0 +1,62 @@
+const urls = [
+  "api.github.com",
+  "assets-cdn.github.com",
+  "avatars.githubusercontent.com",
+  "avatars0.githubusercontent.com",
+  "avatars1.githubusercontent.com",
+  "avatars2.githubusercontent.com",
+  "avatars3.githubusercontent.com",
+  "avatars4.githubusercontent.com",
+  "avatars5.githubusercontent.com",
+  "camo.githubusercontent.com",
+  "central.github.com",
+  "codeload.github.com",
+  "desktop.githubusercontent.com",
+  "favicons.githubusercontent.com",
+  "gist.github.com",
+  "github-cloud.s3.amazonaws.com",
+  "github-com.s3.amazonaws.com",
+  "github-production-release-asset-2e65be.s3.amazonaws.com",
+  "github-production-repository-file-5c1aeb.s3.amazonaws.com",
+  "github-production-user-asset-6210df.s3.amazonaws.com",
+  "github.com",
+  "github.community",
+  "github.githubassets.com",
+  "github.global.ssl.fastly.net",
+  "github.io",
+  "github.map.fastly.net",
+  "githubstatus.com",
+  "media.githubusercontent.com",
+  "raw.githubusercontent.com",
+  "user-images.githubusercontent.com",
+];
+
+const promises = urls.map(async (url) => {
+  const ips = await Deno.resolveDns(url, "A");
+  return { url, ip: ips?.[0] };
+});
+
+const result = await Promise.all(promises);
+
+const hosts = result.map((o) => {
+  return o.ip ? `${o.ip} ${o.url}` : `# ${o.url} ip error`;
+}).join("\n");
+
+console.log(hosts);
+
+const readme = `# Hosts
+
+Improve access speed for github in mainland China
+
+## Last github hosts
+
+Last update: ${new Date().toLocaleString()}
+
+\`\`\`
+${hosts}
+\`\`\`
+`;
+
+const encoder = new TextEncoder();
+Deno.writeFileSync("hosts", encoder.encode(hosts));
+Deno.writeFileSync("README.md", encoder.encode(readme));
